15be6309063e023646dbb1b5b626c5303fc34483
Илья <75577657+Acejkee@users.noreply.github.com>
2021-08-29 18:18:51 +0500

    Update unpacking.py

--- File IO/unpacking/unpacking.py
+++ File IO/unpacking/unpacking.py
@@ -1,10 +1,12 @@
 """
+
 Напишите программу, которая считывает из файла строку, 
 соответствующую тексту, сжатому с помощью кодирования повторов,
 и производит обратную операцию, получая исходный текст.
+
 """
 
-with open(r'C:\Users\belou\Desktop\Python\Stepic\unpacking\dataset.txt') as data:
+with open(***path to file***) as data:
     for i in data:
         for j in range(len(i)):
             if i[j].isalpha():
